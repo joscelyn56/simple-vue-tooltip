62efacb494592f2900c1fc4b6a4c87b5af114bfd
Okwu Joscelyn Kinikachi <ojk564@gmail.com>
2020-04-09 13:06:22 +0100

    Add arrow pointers and event listeners

--- README.md
+++ README.md
@@ -25,19 +25,38 @@ Insert into html element using vue directive e.g button
 <button
   v-tooltip="{
     tip:'This is a clickable button',
-    position: 'bottom'
+    position: 'bottom',
+    event: 'click',
+    action: clicked,
+    actionArgs: [2]
   }"
   ...
 >
   Click Me
 </button>
+
+...
+export default {
+  methods: {
+    clicked (id) {
+      console.log(id)
+    }
+  }
+}
+...
 ```
 
 The v-tooltip directive accepts an object containing 2 properties:
 * tip: a custom message to display
 * position: this specifies the position to display tip relative to the containing element
+* event: name of javascript event e.g. click, mouseover etc
+* action: function to fire when event trigger
+* actionArgs: an array function parameters in the correct defined order
 
 **Below are the list of positions currently available**
+* top: this aligns the tooltip to top center of element
+* top-left: this aligns the tooltip to top left of element
+* top-right: this aligns the tooltip to top right of element
 * bottom: this aligns the tooltip to bottom center of element
 * bottom-left: this aligns the tooltip to bottom left of element
 * bottom-right: this aligns the tooltip to bottom right of element
--- src/index.js
+++ src/index.js
@@ -3,63 +3,146 @@ export default {
         Vue.directive('tooltip', {
             inserted(el, binding, vnode) {
                 const fragment = document.createDocumentFragment()
-            
+        
                 const element = el.cloneNode(true)
-                fragment.appendChild(element)
+                if(binding.value.event && binding.value.action){
+                    let event = binding.value.event
+                    let action = binding.value.action
             
+                    let actionArgs = ""
+                    if(binding.value.actionArgs){
+                        actionArgs = binding.value.actionArgs
+                    }
+                    element.addEventListener(event, function(){
+                        action(actionArgs)
+                    })
+                }
+        
+                fragment.appendChild(element)
+        
                 const tip = document.createElement('div')
                 tip.setAttribute('class', 'tip')
-                tip.style.display = 'none';
                 tip.style.background = '#4d4d4d';
                 tip.style.color = '#FFFFFF';
                 tip.style.fontWeight = 'normal';
                 tip.style.position = 'absolute';
                 tip.style.width = 'max-content';
-                tip.style.padding = '5px 7px';
-                tip.style.borderRadius = '6px';
-                tip.style.zIndex = '2';
+                tip.style.padding = '5px 10px';
+                tip.style.borderRadius = '4px';
+                tip.style.zIndex = 2;
                 tip.style.textTransform = 'capitalize';
                 tip.style.fontSize = '12px';
+        
+                tip.innerText = (binding.value.tip) ? binding.value.tip : 'No tip specified'
+        
+                const arrow = document.createElement('div')
+                arrow.setAttribute('class', 'arrow')
+                arrow.style.width = 0
+                arrow.style.height = 0
+        
+                const tipGroup = document.createElement('div')
+                tipGroup.style.display = 'none'
+                tipGroup.style.position = 'absolute'
+                tipGroup.style.left = 0
+                tipGroup.style.right = 0
+                tipGroup.style.margin = 'auto'
+                tipGroup.style.justifyContent = 'center'
+        
+                if(binding.value.position) {
+                    if (binding.value.position === 'top') {
+                        tipGroup.style.top = '-10px'
+                
+                        arrow.style.borderRight = '6px solid transparent'
+                        arrow.style.borderLeft = '6px solid transparent'
+                        arrow.style.borderTop = '8px solid #4d4d4d'
+                
+                        tip.style.top = '-25px'
+                        tip.style.left = '50%'
+                        tip.style.transform = 'translateX(-50%)'
+                    }
             
-                tip.innerText = (binding.value.tip) ? binding.value.tip : 'Specify tip'
+                    if (binding.value.position === 'top-left') {
+                        tipGroup.style.top = '-10px'
+                
+                        arrow.style.borderRight = '6px solid transparent'
+                        arrow.style.borderLeft = '6px solid transparent'
+                        arrow.style.borderTop = '8px solid #4d4d4d'
+                
+                        tip.style.top = '-25px'
+                        tip.style.left = '-5px'
+                    }
             
-                if (binding.value.position === 'bottom') {
-                    tip.style.top = '25px'
-                    tip.style.left = '50%'
-                    tip.style.transform = 'translateX(-50%)'
-                }
+                    if (binding.value.position === 'top-right') {
+                        tipGroup.style.top = '-10px'
+                
+                        arrow.style.borderRight = '6px solid transparent'
+                        arrow.style.borderLeft = '6px solid transparent'
+                        arrow.style.borderTop = '8px solid #4d4d4d'
+                
+                        tip.style.top = '-25px'
+                        tip.style.left = 'unset'
+                        tip.style.right = '50%'
+                        tip.style.transform = 'translateX(10%)'
+                    }
             
-                if (binding.value.position === 'bottom-left') {
-                    tip.style.top = '25px'
-                    tip.style.left = '0'
-                    tip.style.transform = 'translateX(0%)'
-                }
+                    if (binding.value.position === 'bottom') {
+                        tip.style.bottom = '-25px'
+                        tip.style.left = '50%'
+                        tip.style.transform = 'translateX(-50%)'
+                    }
             
-                if (binding.value.position === 'bottom-right') {
-                    tip.style.top = '25px'
+                    if (binding.value.position === 'bottom-left') {
+                        tip.style.bottom = '-25px'
+                        tip.style.left = 0
+                        tip.style.transform = 'translateX(0%)'
+                    }
+            
+                    if (binding.value.position === 'bottom-right') {
+                        tipGroup.style.bottom = '-10px'
+                
+                        arrow.style.borderRight = '6px solid transparent'
+                        arrow.style.borderLeft = '6px solid transparent'
+                        arrow.style.borderBottom = '8px solid #4d4d4d'
+                
+                        tip.style.bottom = '-25px'
+                        tip.style.right = '-5px'
+                    }
+                }else {
+                    tipGroup.style.bottom = '-10px'
+            
+                    arrow.style.borderRight = '6px solid transparent'
+                    arrow.style.borderLeft = '6px solid transparent'
+                    arrow.style.borderBottom = '8px solid #4d4d4d'
+            
+                    tip.style.bottom = '-25px'
                     tip.style.left = 'unset'
-                    tip.style.right = '50%'
-                    tip.style.transform = 'translateX(10%)'
+                    tip.style.right = '-5px'
                 }
-            
-                fragment.appendChild(tip)
-            
+        
+                const tipFragment = document.createDocumentFragment()
+                tipFragment.appendChild(arrow)
+                tipFragment.append(tip)
+        
+                tipGroup.append(tipFragment)
+        
+                fragment.appendChild(tipGroup)
+        
                 const tooltip = document.createElement('div')
                 tooltip.setAttribute('class', 'element')
                 tooltip.style.position = 'relative';
-            
+        
                 tooltip.addEventListener('mouseenter', function () {
                     let elementTip = this.children[1]
-                    elementTip.style.display = 'block'
+                    elementTip.style.display = 'flex'
                 })
-            
+        
                 tooltip.addEventListener('mouseleave', function () {
                     let elementTip = this.children[1]
                     elementTip.style.display = 'none'
                 })
-            
+        
                 tooltip.append(fragment)
-            
+        
                 vnode.elm.parentElement.replaceChild(tooltip, el);
             }
         })
